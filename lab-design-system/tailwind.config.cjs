496caedb7b857a9e20ba9db14d8edb79c209f0f3
Renan <renan.fachin@hotmail.com>
2022-10-11 23:40:44 -0300

    imports do tailwindcss e no app.tsx. Exemplo do uso do tailwindcss

--- lab-design-system/tailwind.config.cjs
+++ lab-design-system/tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     // Aqui ficará contido os arquivos que vão ter classes do tailwind
-    './src/**/*.tsx'
+    './src/**/*.tsx',
   ],
   theme: {
     extend: {},
