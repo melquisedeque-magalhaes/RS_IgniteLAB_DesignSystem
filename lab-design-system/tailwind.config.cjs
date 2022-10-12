12313fc8ddb9c90f6d267f63358d44e27a7417d0
Renan <renan.fachin@hotmail.com>
2022-10-12 00:56:30 -0300

    setting color tokens in tailwind

--- lab-design-system/tailwind.config.cjs
+++ lab-design-system/tailwind.config.cjs
@@ -5,6 +5,17 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    colors: {
+      // Definindo as cores que o tailwind vai ter, com base no designSystem
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#9be1fb',
+    },
     extend: {
       // Definindo a fonte "padrão"
       fontFamily: {
